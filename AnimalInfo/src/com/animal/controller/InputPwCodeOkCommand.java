ef9f62297b7b4f129c292d38a3e9cf49acff61fc
Bongjae <kbjkim@naver.com>
2022-07-26 20:55:17 +0900

    review 주석추가 -ing-

---
+++ AnimalInfo/src/com/animal/controller/InputPwCodeOkCommand.java
@@ -0,0 +1,25 @@
+package com.animal.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.animal.service.MemberService;
+
+public class InputPwCodeOkCommand implements Command{
+	
+	MemberService memberService = MemberService.getInstance(); // MemberService 정보를 싱글톤 형태로 가져옴.
+	
+	@Override
+	public void excute(HttpServletRequest request, HttpServletResponse response) {
+		int result = 0;
+		
+		String inputCode = request.getParameter("inputCode"); // 웹페이지에서 입력한 inputcode 불러옴.
+		String code = request.getParameter("code"); // 메일로 받은 code와 비교할 code 입력한 값 불러옴.
+		String id = request.getParameter("id"); // id 입력 값 불러옴.
+		result = memberService.checkCode(code, inputCode); // MemberService 에 checkCode 메소드 호출하여 
+		   												   //code , inputcode 매개변수로 넣고 수행결과 result 에 대입.
+		
+		request.setAttribute("result", result); // result 형태로 값 저장.
+		request.setAttribute("id", id); // id 형태로 값 저장.
+	}
+}
