b6c0abe364a0fa3eb1f5454a5eb582d3a2f61eec
Bongjae <kbjkim@naver.com>
2022-07-26 19:01:46 +0900

    혁명

--- AnimalSafe/src/com/animal/exception/IdDuplicatedException.java
+++
@@ -1,8 +0,0 @@
-package com.animal.exception;
-
-@SuppressWarnings("serial")
-public class IdDuplicatedException extends RuntimeException{
-	public IdDuplicatedException(String msg) {
-		super(msg);
-	}
-}
